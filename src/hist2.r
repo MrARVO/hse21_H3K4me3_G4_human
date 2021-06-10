9a687eeb5da631f270f9b914f01a8ce43494b87c
MrARVO <voytetskiyartem@gmail.com>
2021-06-10 07:54:56 +0300

    Update hist2.r

--- src/hist2.r
+++ src/hist2.r
@@ -6,9 +6,7 @@ library(dplyr)
 ###
 
 #NAME <- 'H3K4me3_H1.ENCFF041HYH.hg19'
-#NAME <- 'H3K4me3_H1.ENCFF041HYH.hg38'
-#NAME <- 'H3K4me3_H1.ENCFF883IEF.hg19'
-#NAME <- 'H3K4me3_H1.ENCFF883IEF.hg38'
+NAME <- 'H3K4me3_H1.ENCFF883IEF.hg19'
 OUT_DIR <- 'images/'
 
 ###
@@ -18,9 +16,15 @@ colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
 bed_df$len <- bed_df$end - bed_df$start
 head(bed_df)
 
+#1
+#bed_df <- bed_df %>%
+#  arrange(-len) %>%
+# filter(len < 5500)
+
+#2
 bed_df <- bed_df %>%
   arrange(-len) %>%
-  filter(len < 50000)
+  filter(len < 8000)
   
 ggplot(bed_df) +
   aes(x = len) +
@@ -31,6 +35,6 @@ ggsave(paste0('len_hist.', NAME, '.filtered.pdf'), path = OUT_DIR)
 
 bed_df %>%
   select(-len) %>%
-  write.table(file='H3K4me3_H1.ENCFF883IEF.hg38.filtered.bed',
+  write.table(file='H3K4me3_H1.ENCFF883IEF.hg19.filtered.bed',
             col.names = FALSE, row.names = FALSE, sep = '\t', quote = FALSE)
 
